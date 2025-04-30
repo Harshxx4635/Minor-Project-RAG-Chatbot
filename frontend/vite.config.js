220dcac44273a372889aa4e5f95bdc0d26740df7
Harshxx4635 <103305293+Harshxx4635@users.noreply.github.com>
2025-04-30 23:00:38 +0545

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,4 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   base: './',
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: ['react-markdown'],
-    }
-  }
 });
